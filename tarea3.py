ad4e7106a819aec34d45fb70dacaeb71530ab40e
Agustinpereira07 <101513567+Agustinpereira07@users.noreply.github.com>
2022-04-23 16:34:11 -0300

    Add files via upload

---
+++ tarea3.py
@@ -0,0 +1,24 @@
+# Bucles [Python]
+# Ejercicios de práctica
+# Autor: Inove Coding School
+# Version: 2.0
+# IMPORTANTE: NO borrar los comentarios
+# que aparecen en verde con el hashtag "#"
+# Ejercicios con bucles "while"
+x = 0
+# Dado el siguiente "while", complete la condicion
+# para que el "while" itere siempre que <x sea menor a 6>
+# Además, complete la línea de código necesaria para que
+# el valor de "x" incremente "1" en cada iteración
+condicion = x
+# reemplace "condicion" por lo que crea necesario
+while condicion:  
+    x = +1
+    
+    if x == 5:
+        print("Valor de x =", x)
+
+        break
+    
+    
+   
