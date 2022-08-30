3c2d5dd51dac295624a702c34bb7e28e553ec40d
Agustinpereira07 <101513567+Agustinpereira07@users.noreply.github.com>
2022-08-30 16:12:07 -0300

    Add files via upload

--- tarea3.py
+++ tarea3.py
@@ -1,24 +1,51 @@
-# Bucles [Python]
+# Bucles [Python
 # Ejercicios de práctica
+
 # Autor: Inove Coding School
 # Version: 2.0
+
 # IMPORTANTE: NO borrar los comentarios
 # que aparecen en verde con el hashtag "#"
-# Ejercicios con bucles "while"
-x = 0
-# Dado el siguiente "while", complete la condicion
-# para que el "while" itere siempre que <x sea menor a 6>
-# Además, complete la línea de código necesaria para que
-# el valor de "x" incremente "1" en cada iteración
-condicion = x
-# reemplace "condicion" por lo que crea necesario
-while condicion:  
-    x = +1
-    
-    if x == 5:
-        print("Valor de x =", x)
 
-        break
+# Ejercicio de secuencias numéricas
+
+# Pedir por consola dos números que representen el principio y fin de una
+# secuencia numérica.
+# Realizar un bucle "for" que recorra esa secuencia armada con "range"
+# y calcule a sumatoria total de todos los números dentro de esa secuencia
+# Tener en cuenta que "range" no incluye el número de "fin" en su secuencia,
+# sino que va hasta el anterior
+
+inicio = int(input('Ingrese el primer número de la secuencia\n'))
+fin = int(input('Ingrese el último número de la secuencia\n'))
+
+
+sumatoria = 0  # Inicializo el contador en 0
+
+lista = []
+for i in range(inicio,fin):
+
+
+    print("numeros:",i)
+
+
+    sumatoria += i
+
     
+
+
+
+
+
+
     
-   
+
+
+print(sumatoria)
+
+
+# for ... in range(....)
+
+# Imprimir el valor de la sumatoria
+
+print("terminamos!")
